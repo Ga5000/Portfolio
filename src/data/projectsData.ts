81ac6038845fe9ce9e4b727abb53bc10ad75fda3
Ga5000 <gbr.lisboa@gmail.com>
2025-02-26 12:46:08 -0300

    Added new archived project (Portfolio)

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -34,11 +34,22 @@ const projects: ProjectInterface[] = [
       ? "A powerful and scalable Blog API built with Java and Spring Boot, designed to manage posts, users, and comments efficiently. It includes user authentication, role-based access control, and RESTful endpoints for seamless integration. The API supports CRUD operations, pagination, and search functionality to enhance content management. It also ensures security with JWT authentication and integrates with a relational database for structured data storage."
       :"Uma API de Blog poderosa e escalável desenvolvida com Java e Spring Boot, projetada para gerenciar posts, usuários e comentários de forma eficiente. Inclui autenticação de usuários, controle de acesso baseado em funções e endpoints RESTful para fácil integração. A API suporta operações CRUD, paginação e funcionalidade de busca para otimizar a gestão de conteúdo. Além disso, garante segurança com autenticação JWT e se integra a um banco de dados relacional para armazenamento estruturado de dados.",
     status: isEnglish ? StatusEN.Active : StatusPT.Ativo,
-    repo: "https://github.com/Ga5000/E-commerce",
+    repo: "#",
     stacks: ["Java", "MySQL", "Spring Boot", "PostMan", "MinioS3"]
   },
   {
     id: 3,
+    title: isEnglish ? "PortFolio" : "Portfolio",
+    description: isEnglish
+      ? "This portfolio website you are currently viewing is designed to showcase my projects and achievements. It evolves over time, receiving updates such as new projects, added features, and ongoing improvements. One of the key highlights is a powerful and scalable Blog API built with Java and Spring Boot, designed to manage posts, users, and comments efficiently. The API includes user authentication, role-based access control, and RESTful endpoints for seamless integration. It supports CRUD operations, pagination, and search functionality, enhancing content management. Security is ensured with JWT authentication, and it integrates with a relational database for structured data storage."
+      : "Este site de portfólio que você está visualizando foi desenvolvido para exibir meus projetos e conquistas. Ele evolui ao longo do tempo, recebendo atualizações como novos projetos, recursos adicionados e melhorias contínuas. Um dos destaques é uma API de Blog poderosa e escalável, construída com Java e Spring Boot, projetada para gerenciar posts, usuários e comentários de forma eficiente. A API inclui autenticação de usuários, controle de acesso baseado em funções e endpoints RESTful para fácil integração. Ela suporta operações CRUD, paginação e funcionalidade de busca, otimizando a gestão de conteúdo. A segurança é garantida com autenticação JWT e ela se integra a um banco de dados relacional para armazenamento estruturado de dados.",
+    status: isEnglish ? StatusEN.Archived : StatusPT.Arquivado,
+    repo: "#",
+    stacks: ["React", "TypeScript", "TailwindCss"]
+  }
+  
+  {
+    id: 4,
     title: isEnglish ? "Sudoku Solver" : "Solvedor de Sudoku",
     description: isEnglish
       ? "A Java-based Sudoku solver featuring a graphical interface built with Java Swing. Users can input numbers into a 9x9 board and press the 'Solve' button to receive an instant solution, leveraging backtracking algorithms to efficiently compute the correct answer."
