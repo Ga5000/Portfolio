9a5b0028258ecdfe09dab6030cfdeaf20c90463e
Ga5000 <gbr.lisboa@gmail.com>
2025-04-11 14:25:49 -0300

    bug: correct bug in mobile layout, now the sidebar is deaulted as closed.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
