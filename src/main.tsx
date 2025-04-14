a23e0b7f97c469241c99369e8de238bbbf25d338
Ga5000 <gbr.lisboa@gmail.com>
2025-04-13 22:13:01 -0300

    feat: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './in18';
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
